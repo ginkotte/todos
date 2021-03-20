894b464c1cb8ee4973095285051f2b32813bcb16
Gustavo Inkotte <gus.inkotte@gmail.com>
2021-03-20 20:41:28 -0300

    done middleware, account creation and get todos

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const { response, request } = require('express');
 const cors = require('cors');
+const { v4: uuidv4 } = require('uuid')
 
 // const { v4: uuidv4 } = require('uuid');
 
@@ -8,18 +10,40 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => users.username === username);
+
+  if (!user) {
+    return response.status(400).json({ error: "User not found!" })
+  }
+
+  request.user = user;
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+  const customerAlredyExists = users.some((user) => users.username === username);
+
+  if (customerAlredyExists) {
+    return response.status(400).json({ error: "Username alredy exists!" })
+  };
+
+  users.push({
+    id: uuidv4, // precisa ser um uuid
+    name: name,
+    username: username,
+    todos: []
+  })
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+
+  return response.json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
